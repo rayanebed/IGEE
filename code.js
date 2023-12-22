00850f7e3e7a430693f1ec59478e118bdac67b15
Rayane <rayaneforbusiness@gmail.com>
2023-12-22 16:52:38 +0100

    modified:   code.js 	modified:   index.html 	modified:   style.css

--- code.js
+++ code.js
@@ -142,40 +142,7 @@ elearning.addEventListener('click', function(){
   window.open('https://elearning.univ-boumerdes.dz/course/index.php?categoryid=9');
 })
 
-// Swiper
-const swiper = new Swiper('.swiper', {
-  // Optional parameters
-  // direction: 'vertical',
-  // slidesPerView: 2,
-  spaceBetween: 15,
-  loop: false,
 
-  // If we need pagination
-  pagination: {
-    el: '.swiper-pagination',
-    clickable: true,
-    dynamicBullets: true,
-  },
-
-  // Navigation arrows
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
-
-  // breakpoints (responsive silder)
-  breakpoints:{
-    0: {
-      slidesPerView: 1,
-    },
-    700: {
-      slidesPerView: 2,
-    },
-  }
-});
-
-// Swiper end
-// script
 
 
 
